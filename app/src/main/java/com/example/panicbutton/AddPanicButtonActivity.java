96fdbd1cf658d1a004e957ebac87394178f942b7
nicholsryang <nicholsryan@users.noreply.github.com>
2019-08-10 23:39:03 -0400

    Added nextScreen() method to AddPanicButtonActivity.java to toggle layout visibility when advancing to next screen using the next button in activity_add_panic_button.xml

--- app/src/main/java/com/example/panicbutton/AddPanicButtonActivity.java
+++ app/src/main/java/com/example/panicbutton/AddPanicButtonActivity.java
@@ -1,6 +1,7 @@
 package com.example.panicbutton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,8 +10,13 @@ import android.widget.EditText;
 import android.widget.Spinner;
 
 public class AddPanicButtonActivity extends AppCompatActivity {
-
-
+    /*
+    private Spinner panicButtonTypeSpinner = (Spinner) findViewById(R.id.panicButtonTypeSpinner);
+    //private String callString = getResources().getStringArray(R.array.panic_button_type_array);
+    private String[] typeArray = getResources().getStringArray(R.array.panic_button_type_array);
+    private String callString = typeArray[0];
+    private String textString = typeArray[1];
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +28,35 @@ public class AddPanicButtonActivity extends AppCompatActivity {
     }
 
     public void createButton(View view) {
-        Intent intent = new Intent(this, Button.class);
         Spinner panicButtonTypeSpinner = (Spinner) findViewById(R.id.panicButtonTypeSpinner);
+        Intent intent = new Intent(this, Button.class);
         EditText editText =(EditText)findViewById(R.id.panicButtonName);
         String panicButtonName = editText.getText().toString();
         editText = (EditText) findViewById(R.id.panicButtonPhoneNumber);
         String panicButtonPhoneNumber = editText.getText().toString();
         String panicButtonType = panicButtonTypeSpinner.getSelectedItem().toString();
         Button button = new Button(panicButtonName, panicButtonPhoneNumber, panicButtonType);
+    }
+
+    public void nextScreen(View view) {
+        Spinner panicButtonTypeSpinner = (Spinner) findViewById(R.id.panicButtonTypeSpinner);
+        //String callString = getResources().getStringArray(R.array.panic_button_type_array);
+        String[] typeArray = getResources().getStringArray(R.array.panic_button_type_array);
+        String callString = typeArray[0];
+        String textString = typeArray[1];
+
+        ConstraintLayout initialLayout = (ConstraintLayout) findViewById(R.id.addPanicButtonLayoutInitial);
+        ConstraintLayout callLayout = (ConstraintLayout) findViewById(R.id.addPanicButtonLayoutCall);
+        ConstraintLayout textLayout = (ConstraintLayout) findViewById(R.id.addPanicButtonLayoutText);
+        String panicButtonType = panicButtonTypeSpinner.getSelectedItem().toString();
+
+        if (panicButtonType.equals(callString)) {
+            initialLayout.setVisibility(View.GONE);
+            callLayout.setVisibility(View.VISIBLE);
+        }
+        else if (panicButtonType.equals(textString)) {
+            initialLayout.setVisibility(View.GONE);
+            textLayout.setVisibility(View.VISIBLE);
+        }
     }
 }
